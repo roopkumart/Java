a482c1a13b488157a1892f14950de67cf7255b26
roopkumart <troopkumar53@gmail.com>
2025-10-22 19:24:25 +0530

    SumAverage program added

--- src/main/java/javaInterviewPrograms/SumAverage.java
+++ src/main/java/javaInterviewPrograms/SumAverage.java
@@ -1,5 +1,24 @@
 package javaInterviewPrograms;
 
-public class SumAverage {
+import java.util.Scanner;
 
+public class SumAverage {
+public static void main (String[] args) {
+	Scanner sc=new Scanner(System.in);
+	System.out.println("enter the elements in array: ");
+int n=sc.nextInt();
+int[] arr=new int[n];
+System.out.println("enter the elementts of array : ");
+for (int i=0;i<n;i++) {
+	arr[i]=sc.nextInt();
 }
+int sum=0;
+for(int num:arr) {
+	sum+=num;
+}
+double average=sum/n;
+System.out.println("sum of elements: "+sum);
+System.out.println("average of elements: "+average);
+}
+}
+
